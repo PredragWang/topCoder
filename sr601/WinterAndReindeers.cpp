dabb3f5869ce36dea6f1b07f949da48612776d5b
garrettWang <guanyuwang.cs@gmail.com>
2013-12-28 14:33:41 -0500

    new commit

--- sr601/WinterAndReindeers.cpp
+++ sr601/WinterAndReindeers.cpp
@@ -79,106 +79,106 @@ lcs(A[0, xa-1], B[0, xb-1]) + lcs(A[ya+1, ], B[yb+1, ]) + <length of C>
 #include <cmath>
 using namespace std;
 class WinterAndReindeers{
-	public:
-		int getNumber(vector<string> allA, vector<string> allB, vector<string> allC);
-	private:
-		vector<vector<int>> lcs(string A, string B);
-		vector<vector<int>> lcsReverse(string A, string B);
-		vector<int> subSeq(string str, string C);
+    public:
+        int getNumber(vector<string> allA, vector<string> allB, vector<string> allC);
+    private:
+        vector<vector<int>> lcs(string A, string B);
+        vector<vector<int>> lcsReverse(string A, string B);
+        vector<int> subSeq(string str, string C);
 };
 int WinterAndReindeers::getNumber(vector<string> allA, vector<string> allB, vector<string> allC){
-	string A("");
-	string B("");
-	string C("");
-	for (string s : allA) A = A + s;
-	for (string s : allB) B = B + s;
-	for (string s : allC) C = C + s;
-	int a = A.length(), b = B.length(), c = C.length();
-	vector<vector<int>> lcsAB = lcs(A, B);
-	vector<vector<int>> rlcsAB = lcsReverse(A, B);
-	vector<int> subSeqAC = subSeq(A, C);
-	vector<int> subSeqBC = subSeq(B, C);
-	int m = subSeqAC.size(), n = subSeqBC.size();
-	int ret = 0;
-	int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
-	int lcsLeft = 0, lcsRight = 0;
-	for (int i = 0; i < m; i += 2) {
-		x1 = subSeqAC[i]; 
-		y1 = subSeqAC[i+1];
-		for (int j = 0; j < n; j += 2) {
-			x2 = subSeqBC[j]; 
-			y2 = subSeqBC[j+1];
-			lcsLeft = ((x1 == 0 || x2 == 0) ? 0 : lcsAB[x1-1][x2-1]);
-			lcsRight = ((y1 == a-1 || y2 == b-1) ? 0 : rlcsAB[y1+1][y2+1]);
-			ret = max(ret, lcsLeft + lcsRight + c);
-		}
-	}
-	return ret;
+    string A("");
+    string B("");
+    string C("");
+    for (string s : allA) A = A + s;
+    for (string s : allB) B = B + s;
+    for (string s : allC) C = C + s;
+    int a = A.length(), b = B.length(), c = C.length();
+    vector<vector<int>> lcsAB = lcs(A, B);
+    vector<vector<int>> rlcsAB = lcsReverse(A, B);
+    vector<int> subSeqAC = subSeq(A, C);
+    vector<int> subSeqBC = subSeq(B, C);
+    int m = subSeqAC.size(), n = subSeqBC.size();
+    int ret = 0;
+    int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
+    int lcsLeft = 0, lcsRight = 0;
+    for (int i = 0; i < m; i += 2) {
+        x1 = subSeqAC[i]; 
+        y1 = subSeqAC[i+1];
+        for (int j = 0; j < n; j += 2) {
+            x2 = subSeqBC[j]; 
+            y2 = subSeqBC[j+1];
+            lcsLeft = ((x1 == 0 || x2 == 0) ? 0 : lcsAB[x1-1][x2-1]);
+            lcsRight = ((y1 == a-1 || y2 == b-1) ? 0 : rlcsAB[y1+1][y2+1]);
+            ret = max(ret, lcsLeft + lcsRight + c);
+        }
+    }
+    return ret;
 }
 vector<vector<int>> WinterAndReindeers::lcs(string A, string B) {
-	int m = A.length();
-	int n = B.length();
-	vector<vector<int>> dp(m, vector<int>(n, 0));
-	int up = 0, left = 0;
-	for (int i = 0; i < m; i ++) {
-		for (int j = 0; j < n; j ++) {
-			if (A[i] == B[j]) {
-				dp[i][j] = (i>0 && j>0 ? dp[i-1][j-1]+1 : 1);
-			}
-			else {
-				up = (i == 0 ? 0 : dp[i-1][j]);
-				left = (j == 0 ? 0 : dp[i][j-1]);
-				dp[i][j] = max(up, left);
-			}
-		}
-	}
-	return dp;
+    int m = A.length();
+    int n = B.length();
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    int up = 0, left = 0;
+    for (int i = 0; i < m; i ++) {
+        for (int j = 0; j < n; j ++) {
+            if (A[i] == B[j]) {
+                dp[i][j] = (i>0 && j>0 ? dp[i-1][j-1]+1 : 1);
+            }
+            else {
+                up = (i == 0 ? 0 : dp[i-1][j]);
+                left = (j == 0 ? 0 : dp[i][j-1]);
+                dp[i][j] = max(up, left);
+            }
+        }
+    }
+    return dp;
 }
 
 vector<vector<int>> WinterAndReindeers::lcsReverse(string A, string B) {
-	int m = A.length();
-	int n = B.length();
-	vector<vector<int>> dp(m, vector<int>(n, 0));
-	int down = 0, right = 0;
-	for (int i = m-1; i >= 0; i --) {
-		for (int j = n-1; j >= 0; j --) {
-			if (A[i] == B[j]) {
-				dp[i][j] = (i<m-1 && j<n-1 ? dp[i+1][j+1]+1 : 1);
-			}
-			else {
-				down = (i == m-1 ? 0 : dp[i+1][j]);
-				right = (j == n-1 ? 0 : dp[i][j+1]);
-				dp[i][j] = max(down, right);
-			}
-		}
-	}
-	return dp;
+    int m = A.length();
+    int n = B.length();
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    int down = 0, right = 0;
+    for (int i = m-1; i >= 0; i --) {
+        for (int j = n-1; j >= 0; j --) {
+            if (A[i] == B[j]) {
+                dp[i][j] = (i<m-1 && j<n-1 ? dp[i+1][j+1]+1 : 1);
+            }
+            else {
+                down = (i == m-1 ? 0 : dp[i+1][j]);
+                right = (j == n-1 ? 0 : dp[i][j+1]);
+                dp[i][j] = max(down, right);
+            }
+        }
+    }
+    return dp;
 }
 
 vector<int> WinterAndReindeers::subSeq(string str, string C){
-	vector<int> dp;
-	int n = str.length();
-	int c = C.length();
-	int ptrC = 0;
-	for (int i = 0; i <= n-c; i++) {
-		if (str[i] != C[0]) {
-			continue;
-		}
-		ptrC = 0;
-		for (int j = i; j < n; j++) {
-			if (str[j] == C[ptrC]) {
-				ptrC ++;
-				if (ptrC == c) {
-					if (dp.size() > 0 && dp.back() == j) {
-						dp.pop_back();
-						dp.pop_back();
-					}
-					dp.push_back(i);
-					dp.push_back(j);
-					break; 
-				}
-			}
-		}
-	}
-	return dp;
+    vector<int> dp;
+    int n = str.length();
+    int c = C.length();
+    int ptrC = 0;
+    for (int i = 0; i <= n-c; i++) {
+        if (str[i] != C[0]) {
+            continue;
+        }
+        ptrC = 0;
+        for (int j = i; j < n; j++) {
+            if (str[j] == C[ptrC]) {
+                ptrC ++;
+                if (ptrC == c) {
+                    if (dp.size() > 0 && dp.back() == j) {
+                        dp.pop_back();
+                        dp.pop_back();
+                    }
+                    dp.push_back(i);
+                    dp.push_back(j);
+                    break; 
+                }
+            }
+        }
+    }
+    return dp;
 }
